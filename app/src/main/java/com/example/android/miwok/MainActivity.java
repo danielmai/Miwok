7aaf7540c2df25543ec5b07fe1c1a9541e317598
Katherine Kuan <kuan.katherine@gmail.com>
2016-01-08 13:29:29 -0800

    Starter code for Lesson 1

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find the View that shows the phrases category
+        View phrases = findViewById(R.id.phrases);
+
+        // Set a click listener on that View
+        phrases.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the phrases View is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Pop up a toast message
+                Toast.makeText(MainActivity.this,
+                        "Open the list of phrases", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
