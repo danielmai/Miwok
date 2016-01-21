45d7bbce0b6ed3bda01ddc45a505c14637c46697
Katherine Kuan <kuan.katherine@gmail.com>
2016-01-20 19:48:26 -0800

    1.5 Create array of phrases

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -9,5 +9,18 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+
+        // Create array of phrases
+        String[] phrases = new String[10];
+        phrases[0] = "Where are you going?";
+        phrases[1] = "What is your name?";
+        phrases[2] = "My name is...";
+        phrases[3] = "How are you feeling?";
+        phrases[4] = "I’m feeling good.";
+        phrases[5] = "Are you coming?";
+        phrases[6] = "Yes, I’m coming.";
+        phrases[7] = "I’m coming.";
+        phrases[8] = "Let’s go.";
+        phrases[9] = "Come here.";
     }
 }
