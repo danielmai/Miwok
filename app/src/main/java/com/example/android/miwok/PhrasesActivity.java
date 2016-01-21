2d797e4aefa9adf7142931cc849d289783e1fecf
Katherine Kuan <kuan.katherine@gmail.com>
2016-01-21 10:38:55 -0800

    1.7 Display list of phrases with ListView and ArrayAdapter

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -2,7 +2,8 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -24,19 +25,23 @@ public class PhrasesActivity extends AppCompatActivity {
         phrases[8] = "Let’s go.";
         phrases[9] = "Come here.";
 
-        // Using a "while loop", print out each element of the array to the log output
-        int index = 0;
-        while (index < phrases.length) {
-            // Print a log message
-            Log.v("PhrasesActivity", "Phrase at position " + index + ": " + phrases[index]);
-            // Increase the index by 1 to go to the next position
-            index++;
-        }
+        // Create an {@link ArrayAdapter}, whose data source is an array of Strings. The
+        // adapter knows how to create list items for each item in the array, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single phrase.
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, phrases);
 
-        // Using a "for loop", print out each element of the array to the log output
-        for (int i = 0; i < phrases.length; i++) {
-            // Print a log message
-            Log.v("PhrasesActivity", "Phrase at position " + i + ": " + phrases[i]);
-        }
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_phrases.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each phrase from the phrases array.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemsAdapter);
     }
 }
