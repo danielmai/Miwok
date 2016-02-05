2e2d20ab289334956b420afacda26044b67ebdc7
Katherine Kuan <kuan.katherine@gmail.com>
2016-02-05 13:33:41 -0800

    1.10 Create WordAdapter to display a list of Word objects

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -2,7 +2,6 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class PhrasesActivity extends AppCompatActivity {
@@ -12,36 +11,30 @@ public class PhrasesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
 
-        // Create array of phrases
-        String[] phrases = new String[10];
-        phrases[0] = "Where are you going?";
-        phrases[1] = "What is your name?";
-        phrases[2] = "My name is...";
-        phrases[3] = "How are you feeling?";
-        phrases[4] = "I’m feeling good.";
-        phrases[5] = "Are you coming?";
-        phrases[6] = "Yes, I’m coming.";
-        phrases[7] = "I’m coming.";
-        phrases[8] = "Let’s go.";
-        phrases[9] = "Come here.";
+        // Create array of words
+        Word[] words = new Word[10];
+        words[0] = new Word("Where are you going?", "minto wuksus");
+        words[1] = new Word("What is your name?", "tinnә oyaase'nә");
+        words[2] = new Word("My name is...", "oyaaset...");
+        words[3] = new Word("How are you feeling?", "michәksәs?");
+        words[4] = new Word("I’m feeling good.", "kuchi achit");
+        words[5] = new Word("Are you coming?", "әәnәs'aa?");
+        words[6] = new Word("Yes, I’m coming.", "hәә’ әәnәm");
+        words[7] = new Word("I’m coming.", "әәnәm");
+        words[8] = new Word("Let’s go.", "yoowutis");
+        words[9] = new Word("Come here.", "әnni'nem");
 
-        // Create an {@link ArrayAdapter}, whose data source is an array of Strings. The
-        // adapter knows how to create list items for each item in the array, using the
-        // simple_list_item_1.xml layout resource defined in the Android framework.
-        // This list item layout contains a single {@link TextView}, which the adapter will set to
-        // display a single phrase.
-        ArrayAdapter<String> itemsAdapter =
-                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, phrases);
+        // Create an {@link WordAdapter}, whose data source is an array of {@link Word}s. The
+        // adapter knows how to create list items for each item in the array.
+        WordAdapter adapter = new WordAdapter(this, words);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
         // activity_phrases.xml layout file.
         ListView listView = (ListView) findViewById(R.id.list);
 
-        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
-        // {@link ListView} will display list items for each phrase from the phrases array.
-        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
-        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-        listView.setAdapter(itemsAdapter);
+        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+        // {@link ListView} will display list items for each item in the {@link Word} array.
+        listView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,48 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * {@link WordAdapter} is an {@link ArrayAdapter} that can provide the layout for each list item
+ * based on a data source which is a list of {@link Word} objects.
+ */
+public class WordAdapter extends ArrayAdapter<Word>  {
+
+    public WordAdapter(Context context, Word[] objects) {
+        super(context, 0, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if an existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID default_text_view.
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the default translation from the currentWord object and set this text on
+        // the default TextView.
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_text_view.
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the Miwok translation from the currentWord object and set this text on
+        // the default TextView.
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView.
+        return listItemView;
+    }
+}
