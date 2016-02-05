988290b08697bd22e7dd135e7422980ccd92a025
Katherine Kuan <kuan.katherine@gmail.com>
2016-02-05 13:59:51 -0800

    1.7 Create a list of Strings and practice with loops

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class PhrasesActivity extends AppCompatActivity {
 
     @Override
@@ -12,31 +14,31 @@ public class PhrasesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_phrases);
 
         // Create array of phrases
-        String[] phrases = new String[10];
-        phrases[0] = "Where are you going?";
-        phrases[1] = "What is your name?";
-        phrases[2] = "My name is...";
-        phrases[3] = "How are you feeling?";
-        phrases[4] = "I’m feeling good.";
-        phrases[5] = "Are you coming?";
-        phrases[6] = "Yes, I’m coming.";
-        phrases[7] = "I’m coming.";
-        phrases[8] = "Let’s go.";
-        phrases[9] = "Come here.";
+        ArrayList<String> phrases = new ArrayList<String>();
+        phrases.add("Where are you going?");
+        phrases.add("What is your name?");
+        phrases.add("My name is...");
+        phrases.add("How are you feeling?");
+        phrases.add("I’m feeling good.");
+        phrases.add("Are you coming?");
+        phrases.add("Yes, I’m coming.");
+        phrases.add("I’m coming.");
+        phrases.add("Let’s go.");
+        phrases.add("Come here.");
 
         // Using a "while loop", print out each element of the array to the log output
         int index = 0;
-        while (index < phrases.length) {
+        while (index < phrases.size()) {
             // Print a log message
-            Log.v("PhrasesActivity", "Phrase at position " + index + ": " + phrases[index]);
+            Log.v("PhrasesActivity", "Phrase at position " + index + ": " + phrases.get(index));
             // Increase the index by 1 to go to the next position
             index++;
         }
 
         // Using a "for loop", print out each element of the array to the log output
-        for (int i = 0; i < phrases.length; i++) {
+        for (int i = 0; i < phrases.size(); i++) {
             // Print a log message
-            Log.v("PhrasesActivity", "Phrase at position " + i + ": " + phrases[i]);
+            Log.v("PhrasesActivity", "Phrase at position " + i + ": " + phrases.get(i));
         }
     }
 }
