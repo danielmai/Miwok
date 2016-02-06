9c30300fd54680d15c38fe166aa903cffc8bee9f
Katherine Kuan <kuan.katherine@gmail.com>
2016-02-05 19:13:10 -0800

    1.14 Show correct image for numbers, family, and colors

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,15 +15,15 @@ public class ColorsActivity extends AppCompatActivity {
 
         // Create list of words
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("red", "weṭeṭṭi"));
-        words.add(new Word("orange", "a’lymca"));
-        words.add(new Word("light yellow", "ṭayayyi"));
-        words.add(new Word("green", "chokokki"));
-        words.add(new Word("blue", "co’ki"));
-        words.add(new Word("brown", "ṭakaakki"));
-        words.add(new Word("black", "kululli"));
-        words.add(new Word("gray", "ṭopoppi"));
-        words.add(new Word("white", "kelelli"));
+        words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red));
+        words.add(new Word("orange", "a’lymca", R.drawable.color_orange));
+        words.add(new Word("light yellow", "ṭayayyi", R.drawable.color_yellow));
+        words.add(new Word("green", "chokokki", R.drawable.color_green));
+        words.add(new Word("blue", "co’ki", R.drawable.color_blue));
+        words.add(new Word("brown", "ṭakaakki", R.drawable.color_brown));
+        words.add(new Word("black", "kululli", R.drawable.color_black));
+        words.add(new Word("gray", "ṭopoppi", R.drawable.color_gray));
+        words.add(new Word("white", "kelelli", R.drawable.color_white));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
