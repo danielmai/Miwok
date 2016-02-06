f4187de7df7c9443bd33527e0793b026e7c56117
Katherine Kuan <kuan.katherine@gmail.com>
2016-02-05 18:56:51 -0800

    1.14 Modify WordAdapter to dynamically set an image on each list item

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -48,6 +49,11 @@ public class WordAdapter extends ArrayAdapter<Word>  {
         // Get the Miwok translation from the currentWord object and set this text on
         // the default TextView.
         miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID image.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        // In the ImageView, display a specific resource drawable.
+        imageView.setImageResource(R.drawable.ic_family);
 
         // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
         // the ListView.
